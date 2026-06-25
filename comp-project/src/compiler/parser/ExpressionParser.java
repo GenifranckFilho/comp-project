301668b7e69b5fe787fabf85b61b88afd1bfb433
rodrigonbjrz <rodrigonbjrz@gmail.com>
2026-06-25 17:22:34 -0300

    feature: expression parser criado, no momente ele está controlando a logica de parser tokens.

---
+++ comp-project/src/compiler/parser/ExpressionParser.java
@@ -0,0 +1,35 @@
+package compiler.parser;
+
+import compiler.lexer.Lexer;
+import compiler.lexer.Token;
+import compiler.lexer.TokenType;
+import java.io.IOException;
+
+public class ExpressionParser {
+    
+    private Lexer lexer;
+    private Token lookahead;
+
+    public ExpressionParser(Lexer lexer) throws IOException {
+        this.lexer = lexer;
+        this.lookahead = lexer.readNextToken(); 
+    }
+
+    private void match(TokenType type) throws IOException {
+        if (lookahead.type() == type) {
+            lookahead = lexer.readNextToken();
+        } else {
+            throw new RuntimeException("Erro Sintático: Esperado o token " + type + 
+                                       " mas foi encontrado '" + lookahead.lexema() + "'");
+        }
+    }
+
+    public String parse() throws IOException {
+        if (lookahead.type() == TokenType.ID) {
+            String lexema = lookahead.lexema();
+            match(TokenType.ID);
+            return lexema;
+        }
+        return "";
+    }
+}
