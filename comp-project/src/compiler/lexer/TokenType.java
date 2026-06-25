04e76c99b87e799d854b3eb02792f7aaa4fdf38c
Genifranck <genifranckfilho017@gmail.com>
2026-06-25 01:02:13 -0300

    fix: fix lexer implementation and add test file

--- comp-project/src/compiler/lexer/TokenType.java
+++ comp-project/src/compiler/lexer/TokenType.java
@@ -1,7 +1,7 @@
 package compiler.lexer;
 
 public enum TokenType {
-    INT, FLOAT, //Palavra-chave
+    INT, FLOAT, //Literais de números
     ID, // Identificador
     AP, FP, //Símbolo reservado
     OP_SOMA, OP_SUB, OP_MULT, OP_DIV, OP_MOD, OP_POW, //Operadores
